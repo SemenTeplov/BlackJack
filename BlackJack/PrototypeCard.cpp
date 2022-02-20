a6aa3c5e9ae55a129bbe149b0f8f3f45503cf0e8
SemenTeplov <eburg-severca@mail.ru>
2022-02-20 21:12:58 +0500

    definition class PrototypeCard and its functions

--- BlackJack/PrototypeCard.cpp
+++ BlackJack/PrototypeCard.cpp
@@ -1,1 +1,37 @@
 #include "PrototypeCard.hpp"
+
+int PrototypeCard::getSix() {
+	return _six;
+};
+
+int PrototypeCard::getSeven() {
+	return _seven;
+};
+
+int PrototypeCard::getEight() {
+	return _eight;
+};
+
+int PrototypeCard::getNine() {
+	return _nine;
+};
+
+int PrototypeCard::getTen() {
+	return _ten;
+};
+
+int PrototypeCard::getKnight() {
+	return _knight;
+};
+
+int PrototypeCard::getQueen() {
+	return _queen;
+};
+
+int PrototypeCard::getKing() {
+	return _king;
+};
+
+int PrototypeCard::getAce() {
+	return _ace;
+};
--- BlackJack/PrototypeCard.hpp
+++ BlackJack/PrototypeCard.hpp
@@ -1,5 +1,31 @@
 #ifndef __PROTOTYPECARD__
 #define __PROTOTYPECARD__
 
+class PrototypeCard {
+public:
+	PrototypeCard() {};
+	~PrototypeCard() {};
+
+	int getSix();
+	int getSeven();
+	int getEight();
+	int getNine();
+	int getTen();
+	int getKnight();
+	int getQueen();
+	int getKing();
+	int getAce();
+
+private:
+	int _six = 6;
+	int _seven = 7;
+	int _eight = 8;
+	int _nine = 9;
+	int _ten = 10;
+	int _knight = 10;
+	int _queen = 10;
+	int _king = 10;
+	int _ace = 11;
+};
 
 #endif //__PROTOTYPECARD__

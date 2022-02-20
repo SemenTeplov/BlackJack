afbe806660ffdb30e8872471a7c9a813d776c923
SemenTeplov <eburg-severca@mail.ru>
2022-02-20 20:50:34 +0500

    definition class Suit

--- BlackJack/Suit.hpp
+++ BlackJack/Suit.hpp
@@ -1,5 +1,23 @@
+#include <string>
+
 #ifndef __SUIT__
 #define __SUIT__
 
+class Suit {
+public:
+	Suit() {};
+	~Suit() {};
+
+	std::string getHeart() {};
+	std::string getDimond() {};
+	std::string getClubs() {};
+	std::string getPeaks() {};
+
+private:
+	std::string herat = "heart";
+	std::string dimond = "dimond";
+	std::string clubs = "clubs";
+	std::string peaks = "peaks";
+};
 
 #endif //__SUIT__

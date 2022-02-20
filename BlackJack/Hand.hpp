5ea00d9498623ba4b47d68ff79c924e620ec4664
SemenTeplov <eburg-severca@mail.ru>
2022-02-20 19:25:35 +0500

    Добавьте файлы проекта.

---
+++ BlackJack/Hand.hpp
@@ -0,0 +1,11 @@
+#ifndef __HAND__
+#define __HAND__
+
+class Hand {
+public:
+
+private:
+
+};
+
+#endif //__HAND__
---
+++ BlackJack/Player.hpp
@@ -0,0 +1,11 @@
+#ifndef __Player__
+#define __Player__
+
+class Player {
+public:
+
+private:
+
+};
+
+#endif //__Player__

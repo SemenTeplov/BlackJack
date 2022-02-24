0d0e46cc882f8ade74bad74d778b5537d55d581a
SemenTeplov <eburg-severca@mail.ru>
2022-02-24 21:16:40 +0500

    Made class Color

--- BlackJack/Color.cpp
+++ BlackJack/Color.cpp
@@ -1,1 +1,17 @@
 #include "Color.hpp"
+
+std::string Color::getWhite() const {
+	return _white;
+};
+
+std::string Color::getRed() const {
+	return _red;
+};
+
+std::string Color::getGreen() const {
+	return _green;
+};
+
+std::string Color::getBlack() const {
+	return _black;
+};
